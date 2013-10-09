d2b06c8034adb7d11cb6f3d5a8c68a26a541395a
Jpsworld <jpsworld.in@gmail.com>
2013-10-09 18:16:11 +0530

    Create Controller : by Jp

---
+++ Controller/LanguageAppController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org) 
+ */
+
+App::uses('Controller', 'Controller');
+App::uses('AppController', 'Controller');
+
+/**
+ * Class LanguageAppController
+ *
+ * Plugin Name  :   Language Translator  
+ * Author       :   Jp
+ */
+
+class LanguageAppController extends AppController {
+
+}
