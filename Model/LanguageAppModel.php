e21bf1b31c0befdbe2d862f77f53d6586ba2bc82
Jpsworld <jpsworld.in@gmail.com>
2013-10-09 18:20:19 +0530

    Create Model : by Jp

---
+++ Model/LanguageAppModel.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org) 
+ */
+
+App::uses('Model', 'Model');
+App::uses('AppModel', 'Model');
+
+/**
+ * Class LanguageAppModel
+ *
+ * Plugin Name  :   Language Translator  
+ * Author       :   Jp
+ */
+
+class LanguageAppModel extends AppModel {
+
+}
